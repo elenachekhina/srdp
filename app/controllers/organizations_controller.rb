f8132284e2f2f3515ae27c69f8141d4f6eef94cd
elenachekhina <elenachekhina@yandex.ru>
2023-11-01 16:06:43 +0400

    Add visitor index / show

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class OrganizationsController < ApplicationController
-  def index; end
+  def index
+    @organizations = Organization.all
+  end
+
+  def show
+    @organization = Organization.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   constraints subdomain: false do
     root to: 'organizations#index'
-    resources :organizations, only: %w[index]
+    resources :organizations, only: %w[index show]
 
     controller :sessions do
       get 'login', to: 'sessions#new'
